80a0025994b7f79423d1b47b0b2206ab99d4dfbf
zuhir <zouhirbakkas@gmail.com>
2023-12-14 20:45:48 +0100

    bbbg

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: zbakkas <zbakkas@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/07 19:44:26 by zbakkas           #+#    #+#             */
-/*   Updated: 2023/12/14 19:57:45 by zbakkas          ###   ########.fr       */
+/*   Updated: 2023/12/14 20:45:08 by zbakkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void	*ft_memchr(const void *str, int c, size_t n)
 	re = str;
 	while (n > x)
 	{
-		if (*((char *)re + x) == c)
+		if (*((unsigned char *)re + x) == (unsigned char)c)
 		{
 			return ((char *)re + x);
 		}
@@ -29,10 +29,10 @@ void	*ft_memchr(const void *str, int c, size_t n)
 	}
 	return (NULL);
 }
-
+/*
 int main ()
 {
 	char *s="hhhh";
 	if(ft_memchr(s,2+256,3) == s+2)
 		printf("ffff");
-}
+}*/
