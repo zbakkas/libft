a7cf6e7807cab28840835c22358cbdbd327c45f2
zuhir <zouhirbakkas@gmail.com>
2023-12-20 21:52:39 +0100

    hhbhb

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: zbakkas <zbakkas@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 19:07:07 by zbakkas           #+#    #+#             */
-/*   Updated: 2023/12/20 14:29:58 by zbakkas          ###   ########.fr       */
+/*   Updated: 2023/12/20 21:52:01 by zbakkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,55 +32,39 @@ int	c_word(char const *s, char c)
 	return (re + 1);
 }
 
-int	*l_word(char const *s, char c, int x)
+void	is_free(char **d)
 {
-	int	*re;
-	int	j;
-	int	xx;
+	int	x;
 
-	j = 0;
-	xx = x;
-	re = malloc(sizeof(int) * 3);
-	while (s[xx] != c && s[xx] != '\0')
+	x = 0;
+	while (d[x])
 	{
-		xx++;
-		j++;
-	}
-	re[0] = j;
-	j = 0;
-	xx = 0;
-	while (s[xx])
-	{
-		if (s[xx] != c)
-			j++;
-		xx++;
-	}
-	re[1] = j;
-	re[2] = 0;
-	return (re);
-}
-
-int	n_word(char const *s, char c, int x)
-{
-	if (s[x] != c && x == 0)
-		return (0);
-	while (s[x] && s[x] != c)
-		x++;
-	while (s[x] == c)
-		x++;
-	return (x);
-}
-
-void	is_free(char const *s, char c, char **d, int x)
-{
-
-	while (x)
-	{
-		x--;
 		free(d[x]);
+		x++;
 	}
 	free(d);
 	d = NULL;
+}
+
+char	*word(char const *s, char c, int *x)
+{
+	int		i;
+	char	*re;
+	int		j;
+
+	i = 0;
+	while (s[*x] == c)
+		(*x)++;
+	while (s[(*x) + i] && s[(*x) + i] != c)
+		i++;
+	re = malloc (i + 1);
+	if (!re)
+		return (NULL);
+	j = 0;
+	while (i > j)
+		re[j++] = s[(*x)++];
+	re[j] = '\0';
+	return (re);
 }
 
 char	**ft_split(char const *s, char c)
@@ -91,24 +75,14 @@ char	**ft_split(char const *s, char c)
 
 	x = 0;
 	j = 0;
-	re = (char **)ft_calloc((1 + c_word(s, c)), sizeof(char *));
+	re = (char **)malloc(1 + c_word(s, c));
 	if (!re)
 		return (NULL);
-	printf("mawssalch\n");
-	if ((c_word(s, c) == 1 && !l_word(s, c, x)[1]) || (!s || c == '\0'))
+	while (j < c_word(s, c))
 	{
-		printf("WSAL\n");
-		re[0] = NULL;
-		return (re);
-	}
-	while (c_word(s, c) > j)
-	{
-		x = n_word(s, c, x);
-		re[j] = ft_substr(s, x, l_word(s, c, x)[0]);
+		re[j] = word(s, c, &x);
 		if (!re[j])
-			is_free(s, c, re, j);
-		if (x == 0)
-			x++;
+			is_free(re);
 		j++;
 	}
 	re[j] = NULL;
@@ -118,20 +92,17 @@ char	**ft_split(char const *s, char c)
 int main ()
 {
 	
-    char const *s ="hello!!hellow!";
+    char const *s ="  hegrtg ll  o!";
 	//char const *s ="  d s vv   ";
-	char c ='!';
-	int x = c_word(s,c); 
+	char c =' ';
+	int x = 0; 
 	int j=0;
-	char **po = ft_split(s, c);
-
-	// while (po[j])
-	// {
-	// 	printf("%s\n",po[j]);
-	// 	j++;
-	// }
-
-
-	printf("%d\n",c_word(s,c));
-	//printf("%s\n",ft_split(s,c)[0]);
+	while (ft_split(s,c)[j])
+	{
+		printf("%s\n",ft_split(s,c)[j]);
+		j++;
+	}
+	//printf("%d\n",c_word(s,c));
+	//printf("%s\n",word(s,c,&x));
+	//printf("%s\n",word(s,c,&x));
 }
