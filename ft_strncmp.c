f9946bff1048ffa9996a30eb938b01d448d8bf4e
Zouhirborali <105161749+Zouhirborali@users.noreply.github.com>
2023-12-24 02:32:38 +0100

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -18,6 +18,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*str1;
 	unsigned char	*str2;
 
+	if (!n)
+		return (0);
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	x = 0;
