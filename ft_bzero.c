b9237e9e08d0fd5cbc89889376e33bd9f783f1a6
Zouhirborali <105161749+Zouhirborali@users.noreply.github.com>
2024-01-19 23:01:31 +0100

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -14,8 +14,18 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	while (n--)
+//	while (n--)
+//	{
+//		*((char *)(s + n)) = 0;
+//	}
+	char	*str;
+	size_t	i;
+
+	str = (char *)s;
+	i = 0;
+	while (i < n)
 	{
-		*((char *)(s + n)) = 0;
+		str[i] = '\0';
+		i++;
 	}
 }
